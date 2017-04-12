e24ab3454a6428c4b6a13a82b175329d0429fbd1
Brendan Ochs <brendanochs@gmail.com>
2017-04-12 11:31:27 -0500

    Output to person

--- CSC4400_GroupProject_Skol_Ochs/Main.cpp
+++ CSC4400_GroupProject_Skol_Ochs/Main.cpp
@@ -46,7 +46,7 @@ void Header(ofstream &Outfile)
 {       // Receives – the output file
 		// Task - Prints the output preamble
 		// Returns - Nothing
-	Outfile << setw(35) << "Brenden Ochs & Micaiah Skolnick ";
+	Outfile << setw(35) << "Person & Micaiah Skolnick ";
 	Outfile << setw(17) << "CSC 40600";
 	Outfile << setw(15) << "Section 11" << endl;
 	Outfile << setw(27) << "Spring 2017";
